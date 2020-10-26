a38653ed9e5b862c4b4fd4ce940f703bc89f2851
wxh0402 <wxh_email@yeah.net>
2020-10-27 02:13:20 +0800

    chore:自动化发布

--- build.bat
+++ build.bat
@@ -4,21 +4,40 @@
 CHCP 65001
 
 echo.
-echo [提示]: 开始打包
+echo [提示]: 开始发布版本
 echo.
 
-
+echo [提示]: 开始生成exe文件
 :: 使用pyinstaller打包
 pyinstaller.exe --hidden-import pkg_resources.py2_warn  --noconsole -F -i ./ResourceFiles/img/logo.ico "main.py"
 
-node release_produce.js
+echo [提示]: 生成结束
 
+if not exist Publish MD Publish
+
+echo [提示]: 开始压缩打包版本
 :: 复制生成的结果后删除临时文件
-COPY /Y "dist\main.exe" "main.exe"
-RMDIR /S /Q "build" "dist" "__pycache__"
+COPY /Y "dist\main.exe" "\publish\main.exe"
+RMDIR /S /Q "build" "dist" "__pycache__" "logs" "'.\Part\__pycache__" ".\PublicReference\__pycache__"
+DEL /Q ".\Publish\*.*"
 DEL /Q "main.spec"
 
+Rar.exe a ".\publish\DNF计算器%date:~8,2%.%date:~11,2%.zip" "\publish\main.exe" "ResourceFiles"
+
+Rar.exe a ".\publish\%date:~8,2%.%date:~11,2%.zip" "ResourceFiles" "main.py" "Part" "PublicReference"
+
+DEL /Q "\publish\main.exe"
+echo [提示]: 打包结束
+
+echo [提示]: 开始记录更新日志
+node release_produce.js
+
+pandoc -s CHANGELOG.md -o ".\publish\更新日志%date:~8,2%.%date:~11,2%.docx"
+
+echo [提示]: 记录结束
 
 echo.
-echo [提示]: 打包结束
+echo [提示]: 发布结束,发布包参见Publish文件夹
 echo.
+
+set /p var=任意键继续:
--- release_produce.js
+++ release_produce.js
@@ -1,89 +1,89 @@
-const child = require("child_process");
-const fs = require("fs");
-
-// const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
-// const output = child
-//   .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
-//   .toString("utf-8");
-
-const output = child
-  .execSync(`git log --format=%B%H----DELIMITER----`)
-  .toString("utf-8");
-
-const commitsArray = output
-  .split("----DELIMITER----\n")
-  .map(commit => {   
-    let data = commit.split("\n");
-    let returndata = ['',''];
-    let i = 0
-    for(i;i<data.length-1;i++){
-       returndata[0]+=data[i]+(i<data.length-2?'<br>':'')
-     }
-    const [message, sha] = [returndata[0],data[data.length-1]];
-    return {sha, message };
-  })
-  .filter(commit => Boolean(commit.sha));
-
-const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
-// const currentVersion = Number(require("./release_version.json").version);
+const currentVersion = (require("./release_version.json").version).toString();
 const newVersion = new Date().toISOString().split("T")[0]
 
-// 用version和时间作为release 标记
-let newChangelog = `# ${
-  new Date().toISOString().split("T")[0]
-}\n\n`;
-
-const features = [];
-const Bugfixes = [];
-
-// 分别维护features和bugfixes的内容，并将message和commit的链接进行绑定
-commitsArray.forEach(commit => {
-  if (commit.message.startsWith("newfeature:")) {
-    features.push(
-      `* ${commit.message.replace("newfeature:", "")} ([${commit.sha.substring(
-        0,
-        6
-      )}](https://github.com/wxh0402/DNFCalculating/commit/${
-        commit.sha
-      }))\n`
-    );
-  }
-  if (commit.message.startsWith("bugfix:")) {
-    Bugfixes.push(
-      `* ${commit.message.replace("bugfix:", "")} ([${commit.sha.substring(
-        0,
-        6
-      )}](https://github.com/wxh0402/DNFCalculating/commit/${
-        commit.sha
-      }))\n`
-    );
-  }
-});
-
-if (features.length) {
-  newChangelog += `## NewFeatures\n`;
-  features.forEach(feature => {
-    newChangelog += feature;
+if(currentVersion!=newVersion){
+  const child = require("child_process");
+  const fs = require("fs");
+  
+  const output = child
+    .execSync(`git log --format=%B%H----DELIMITER----`)
+    .toString("utf-8");
+  
+  const commitsArray = output
+    .split("----DELIMITER----\n")
+    .map(commit => {   
+      let data = commit.split("\n");
+      let returndata = ['',''];
+      let i = 0
+      for(i;i<data.length-1;i++){
+         returndata[0]+=data[i]+(i<data.length-2?'<br>\n':'')
+       }
+      const [message, sha] = [returndata[0],data[data.length-1]];
+      return {sha, message };
+    })
+    .filter(commit => Boolean(commit.sha));
+  
+  const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
+  
+  
+  // 用version和时间作为release 标记
+  let newChangelog = `# ${
+    new Date().toISOString().split("T")[0]
+  }\n\n`;
+  
+  const features = [];
+  const Bugfixes = [];
+  
+  // 分别维护features和bugfixes的内容，并将message和commit的链接进行绑定
+  commitsArray.forEach(commit => {
+    if (commit.message.startsWith("newfeature:")) {
+      features.push(
+        `* ${commit.message.replace("newfeature:", "")} ([${commit.sha.substring(
+          0,
+          6
+        )}](https://github.com/wxh0402/DNFCalculating/commit/${
+          commit.sha
+        }))\n`
+      );
+    }
+    if (commit.message.startsWith("bugfix:")) {
+      Bugfixes.push(
+        `* ${commit.message.replace("bugfix:", "")} ([${commit.sha.substring(
+          0,
+          6
+        )}](https://github.com/wxh0402/DNFCalculating/commit/${
+          commit.sha
+        }))\n`
+      );
+    }
   });
-  newChangelog += '\n';
+  
+  if (features.length) {
+    newChangelog += `## NewFeatures\n`;
+    features.forEach(feature => {
+      newChangelog += feature;
+    });
+    newChangelog += '\n';
+  }
+  
+  if (Bugfixes.length) {
+    newChangelog += `## BugFixes\n`;
+    Bugfixes.forEach(bugfix => {
+      newChangelog += bugfix;
+    });
+    newChangelog += '\n';
+  }
+  
+  // prepend the newChangelog to the current one
+  fs.writeFileSync("./CHANGELOG.md", `${newChangelog}${currentChangelog}`);
+  // update package.json
+  fs.writeFileSync("./release_version.json", JSON.stringify({ version: String(newVersion) }, null, 2));
+  
+  // create a new commit
+  child.execSync('git add .');
+  child.execSync(`git commit -m "chore: Bump to  ${newVersion}"`);
+  
+  // tag the commit
+  child.execSync(`git tag -a -m "Tag for ${newVersion}" ${newVersion}`);
+
 }
-
-if (Bugfixes.length) {
-  newChangelog += `## BugFixes\n`;
-  Bugfixes.forEach(bugfix => {
-    newChangelog += bugfix;
-  });
-  newChangelog += '\n';
-}
-
-// prepend the newChangelog to the current one
-fs.writeFileSync("./CHANGELOG.md", `${newChangelog}${currentChangelog}`);
-// update package.json
-fs.writeFileSync("./release_version.json", JSON.stringify({ version: String(newVersion) }, null, 2));
-
-// create a new commit
-child.execSync('git add .');
-child.execSync(`git commit -m "chore: Bump to  ${newVersion}"`);
-
-// tag the commit
-child.execSync(`git tag -a -m "Tag for ${newVersion}" ${newVersion}`);
