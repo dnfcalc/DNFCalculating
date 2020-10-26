3c2f11de904da98e44e71b2a4127849073068b8e
西瓜 <@DESKTOP-TR0E16A>
2020-10-26 19:53:02 +0800

    chore: Bump to version 2020-10-26

--- release_produce.js
+++ release_produce.js
@@ -1,31 +1,51 @@
 const child = require("child_process");
 const fs = require("fs");
 
-// const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
-// const output = child
-//   .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
-//   .toString("utf-8");
-
+const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
 const output = child
-  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
   .toString("utf-8");
+
+// const output = child
+//   .execSync(`git log --format=%B%H----DELIMITER----`)
+//   .toString("utf-8");
 
 const commitsArray = output
   .split("----DELIMITER----\n")
-  .map(commit => {
-    const [message, sha] = commit.split("\n");
-
-    return { sha, message };
+  .map(commit => {   
+    let data = commit.split("\n");
+    let returndata = ['',''];
+    let i = 0
+    for(i;i<data.length-1;i++){
+       returndata[0]+=data[i]+(i<data.length-2?'<br>':'')
+     }
+    const [message, sha] = [returndata[0],data[data.length-1]];
+    // let retmessage = "测试"
+    // for(let i =0;i<msg.length-1;i++){
+    //   retmessage+=msg[i]+i<msg.length-2?'\n':''
+    // }
+    // let retsha = msg[msg.length-1]
+    // const [message, sha] = [retmessage,retsha]
+    return {sha, message };
   })
   .filter(commit => Boolean(commit.sha));
 
 const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
-const currentVersion = Number(require("./release_version.json").version);
+// const currentVersion = Number(require("./release_version.json").version);
 const newVersion = new Date().toISOString().split("T")[0]
 
 // 用version和时间作为release 标记
-let newChangelog = `${
-  new Date().toISOString().split("T")[0]
+var now = new Date();
+var year = now.getFullYear(); //得到年份
+var month = now.getMonth();//得到月份
+var date = now.getDate();//得到日期
+month = month + 1;
+if (month < 10) month = "0" + month;
+if (date < 10) date = "0" + date;
+var time = "";
+time = year + "." + month + "." + date;
+let newChangelog = `# ${
+  time
 }\n\n`;
 
 const features = [];
@@ -33,22 +53,22 @@ const Bugfixes = [];
 
 // 分别维护features和bugfixes的内容，并将message和commit的链接进行绑定
 commitsArray.forEach(commit => {
-  if (commit.message.startsWith("newfeature: ")) {
+  if (commit.message.startsWith("newfeature:")) {
     features.push(
-      `* ${commit.message.replace("newfeature: ", "")} ([${commit.sha.substring(
+      `* ${commit.message.replace("newfeature:", "")} ([${commit.sha.substring(
         0,
         6
-      )}](https://github.com/BaronStack/release_notes/commit/${
+      )}](https://github.com/wxh0402/DNFCalculating/commit/${
         commit.sha
       }))\n`
     );
   }
-  if (commit.message.startsWith("bugfix: ")) {
+  if (commit.message.startsWith("bugfix:")) {
     Bugfixes.push(
-      `* ${commit.message.replace("bugfix: ", "")} ([${commit.sha.substring(
+      `* ${commit.message.replace("bugfix:", "")} ([${commit.sha.substring(
         0,
         6
-      )}](https://github.com/BaronStack/release_notes/commit/${
+      )}](https://github.com/wxh0402/DNFCalculating/commit/${
         commit.sha
       }))\n`
     );
@@ -56,7 +76,7 @@ commitsArray.forEach(commit => {
 });
 
 if (features.length) {
-  newChangelog += `## NewFeature\n`;
+  newChangelog += `## NewFeatures\n`;
   features.forEach(feature => {
     newChangelog += feature;
   });
@@ -64,8 +84,8 @@ if (features.length) {
 }
 
 if (Bugfixes.length) {
-  newChangelog += `## Bugfixes\n`;
-  bugfixes.forEach(bugfix => {
+  newChangelog += `## BugFixes\n`;
+  Bugfixes.forEach(bugfix => {
     newChangelog += bugfix;
   });
   newChangelog += '\n';
