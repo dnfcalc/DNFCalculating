69da0dda1bd8931edfce37451d09fa96d02bcdeb
西瓜 <@DESKTOP-TR0E16A>
2020-10-26 19:58:28 +0800

    chore: Bump to version 2020-10-26

--- release_produce.js
+++ release_produce.js
@@ -1,14 +1,14 @@
 const child = require("child_process");
 const fs = require("fs");
 
-const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
-const output = child
-  .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
-  .toString("utf-8");
-
+// const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
 // const output = child
-//   .execSync(`git log --format=%B%H----DELIMITER----`)
+//   .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
 //   .toString("utf-8");
+
+const output = child
+  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .toString("utf-8");
 
 const commitsArray = output
   .split("----DELIMITER----\n")
@@ -32,23 +32,12 @@ const commitsArray = output
 
 const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
 // const currentVersion = Number(require("./release_version.json").version);
-
+const newVersion = new Date().toISOString().split("T")[0]
 
 // 用version和时间作为release 标记
-var now = new Date();
-var year = now.getFullYear(); //得到年份
-var month = now.getMonth();//得到月份
-var date = now.getDate();//得到日期
-month = month + 1;
-if (month < 10) month = "0" + month;
-if (date < 10) date = "0" + date;
-var time = "";
-time = year + "." + month + "." + date;
 let newChangelog = `# ${
-  time
+  new Date().toISOString().split("T")[0]
 }\n\n`;
-
-const newVersion = time
 
 const features = [];
 const Bugfixes = [];
