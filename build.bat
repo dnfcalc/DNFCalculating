221d3bcda207b789dbfd43d60d06844c68082eed
西瓜 <@DESKTOP-TR0E16A>
2020-10-27 18:26:06 +0800

    chore: Bump to  2020-10-27

--- build.bat
+++ build.bat
@@ -4,6 +4,26 @@
 CHCP 65001
 
 echo.
+echo     此文件为自动编译计算器为exe程序脚本,需提前安装以下依赖
+echo       - python
+echo       - pyinstaller
+echo       - PyQt5
+echo     如需自动根据submit更新CHANGELOG并生成更新日志,需提前安装以下依赖
+echo       - node.js
+echo       - pandoc
+echo     如需自动上传到蓝奏云,需要提前安装以下依赖
+echo       - lanzou-api
+echo.
+
+pause
+
+@REM 检查python
+
+@REM for /f "tokens=2" %%h in ('python -h ^| findstr /C:"usage:"') do ^
+@REM set PYVER2=%%h
+@REM if "%PYVER2%" != "python" (@echo 未安装python)
+
+echo.
 echo [提示]: 开始发布版本
 echo.
 
@@ -11,36 +31,38 @@ echo [提示]: 开始生成exe文件
 ::使用pyinstaller打包
 pyinstaller.exe --hidden-import pkg_resources.py2_warn  --noconsole -F -i ./ResourceFiles/img/logo.ico "main.py"
 
-DEL /Q "Bulid\Publish\*.*"
+DEL /Q "AutoRelese\Publish\*.*"
 DEL /Q "main.spec"
 COPY /Y "dist\main.exe" "main.exe"
 RMDIR /S /Q "build" "dist" "__pycache__" "logs" ".\Part\__pycache__" ".\PublicReference\__pycache__"
+for /d %%i in (ResourceFiles\*) do if exist "%%i\set" RMDIR /s /q "%%i\set"
 
 echo [提示]: 生成结束
 
-if not exist Bulid\Publish MD Bulid\Publish
+if not exist AutoRelese\Publish MD AutoRelese\Publish
 
-if exist Bulid\Rar.exe echo [提示]: 开始压缩打包版本
+if exist AutoRelese\Rar.exe echo [提示]: 开始压缩打包版本
 ::复制生成的结果后删除临时文件
 ::rar压缩
-if exist Bulid\Rar.exe Bulid\Rar.exe a "Bulid\publish\DNF计算器%Date:~5,2%.%Date:~8,2%.zip" "main.exe" "ResourceFiles"
-if exist Bulid\Rar.exe Bulid\Rar.exe a "Bulid\publish\源码%Date:~5,2%.%Date:~8,2%.zip" "ResourceFiles" "main.py" "Part" "PublicReference"
-if exist Bulid\Rar.exe DEL /Q "main.exe"
-if exist Bulid\Rar.exe echo [提示]: 打包结束
+if exist AutoRelese\Rar.exe AutoRelese\Rar.exe a "AutoRelese\publish\DNF计算器%Date:~5,2%.%Date:~8,2%.zip" "main.exe" "ResourceFiles"
+if exist AutoRelese\Rar.exe AutoRelese\Rar.exe a "AutoRelese\publish\源码%Date:~5,2%.%Date:~8,2%.zip" "ResourceFiles" "main.py" "Part" "PublicReference"
+if exist AutoRelese\Rar.exe DEL /Q "main.exe"
+if exist AutoRelese\Rar.exe echo [提示]: 打包结束
 
-if exist Bulid\release_produce.js echo [提示]: 开始记录更新日志
-if exist Bulid\release_produce.js node Bulid/release_produce.js
+if exist AutoRelese\release_produce.js echo [提示]: 开始记录更新日志
+if exist AutoRelese\release_produce.js node AutoRelese/release_produce.js
 ::pandoc将md转换为docx
-if exist Bulid\release_produce.js pandoc -s CHANGELOG.md -o "Bulid\publish\更新日志%Date:~5,2%.%Date:~8,2%.docx"
-if exist Bulid\release_produce.js echo [提示]: 记录结束
+if exist AutoRelese\release_produce.js pandoc -s CHANGELOG.md -o "AutoRelese\publish\更新日志%Date:~5,2%.%Date:~8,2%.docx"
+if exist AutoRelese\release_produce.js echo [提示]: 记录结束
 
-if exist Bulid\upload_lanzouyun.py echo [提示]: 开始上传到网盘
-if exist Bulid\upload_lanzouyun.py python upload_lanzouyun.py
-if exist Bulid\upload_lanzouyun.py echo [提示]: 上传结束
+if exist AutoRelese\upload_lanzouyun.py echo [提示]: 开始上传到网盘
+if exist AutoRelese\upload_lanzouyun.py python AutoRelese/upload_lanzouyun.py
+if exist AutoRelese\upload_lanzouyun.py echo [提示]: 上传结束
 
 echo.
-echo [提示]: 发布结束,发布包参见Bulid\Publish文件夹
+echo [提示]: 发布结束,发布包参见AutoRelese\Publish文件夹
 echo.
 
 
 set /p var=任意键继续:
+pause
