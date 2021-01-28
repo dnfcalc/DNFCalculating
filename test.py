ac9b2b1c1ea32ab16fe8f939b2187e8ef566585c
wxh0402 <wxh_email@yeah.net>
2021-01-28 10:59:00 +0800

    chore:移除多余文件

--- test.py
+++
@@ -1,9 +0,0 @@
-def faction(lis):
-    for i in lis:
-        if isinstance(i,list):
-            faction(i)
-        else:
-            print(i)
-
-lis = [1, 2, [4, [5, [6, [7], 8],9],0],3,9]
-faction(lis)
