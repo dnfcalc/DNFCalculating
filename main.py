7076b956a4ce34243c23aed5a179d927e10b43c0
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2021-03-25 00:27:52 +0800

    chore:调整bug反馈地址

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ class 选择窗口(QWidget):
         action_0 = QAction('设置',parent=menu)
         action_0.triggered.connect(lambda state:self.openSet())
         action_1 = QAction('BUG反馈-Gitee',parent=menu)
-        action_1.triggered.connect(lambda state:self.打开链接(['https://gitee.com/i_melon/DNFCalculating/issues']))
+        action_1.triggered.connect(lambda state:self.打开链接(['https://gitee.com/i_melon/DNFCalculating/issues?state=all']))
         action_2 = QAction('联系我们-QQ-1群',parent=menu)
         action_2.triggered.connect(lambda state:self.打开链接(['https://jq.qq.com/?_wv=1027&k=VSNtZ1xv']))
         action_3 = QAction('联系我们-QQ-2群',parent=menu)
