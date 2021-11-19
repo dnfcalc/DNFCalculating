7bab38e45fad1fd9b3cfb704df713bb01ea03e1c
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2021-11-19 22:28:46 +0800

    chore:优化uniqueCode存储

--- PublicReference/utils/uniqueCode.py
+++ PublicReference/utils/uniqueCode.py
@@ -1,13 +1,19 @@
 import hashlib
 import sys
 import wmi
+from .storex import store
 
 
 def get_mac_address():
-    if "main.py" not in sys.argv[0]:
+    if "main.py" in sys.argv[0]:
         try:
-            w = wmi.WMI()
-            return hashlib.md5(w.Win32_DiskDrive()[0].SerialNumber.encode(encoding='UTF-8')).hexdigest()
+            uniqueCode = store.get("uniqueCode")
+            print(uniqueCode)
+            if uniqueCode == None:
+                w = wmi.WMI()
+                uniqueCode = hashlib.md5(w.Win32_DiskDrive()[0].SerialNumber.encode(encoding='UTF-8')).hexdigest()
+                store.set("uniqueCode",uniqueCode)
+            return uniqueCode
         except:
             return ''
     else:
