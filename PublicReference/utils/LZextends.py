bfdc50bdddfbcd063f1af13b0c0fce31da3276f9
西瓜° <wxh_email@yeah.net>
2021-12-02 16:34:20 +0800

    chore:源码修改

--- PublicReference/utils/LZextends.py
+++ PublicReference/utils/LZextends.py
@@ -12,10 +12,10 @@ class LZextends():
     def _all_possible_urls(url: str) -> List[str]:
         """蓝奏云的主域名有时会挂掉, 此时尝试切换到备用域名"""
         available_domains = [
-            'lanzouo.com',  # 鲁ICP备15001327号-8, 2021-09-15
+            # 'lanzouo.com',  # 鲁ICP备15001327号-8, 2021-09-15
             'lanzouw.com',  # 鲁ICP备15001327号-7, 2021-09-02
             'lanzoui.com',  # 鲁ICP备15001327号-6, 2020-06-09, SEO 排名最低
             'lanzoux.com',  # 鲁ICP备15001327号-5, 2020-06-09
             'lanzous.com'  # 主域名, 备案异常, 部分地区已经无法访问
         ]
-        return [url.replace('lanzous.com', d) for d in available_domains]
+        return [url.replace('lanzouo.com', d) for d in available_domains]
