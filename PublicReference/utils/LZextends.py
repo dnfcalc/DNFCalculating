3a363dfae5774026f8d8d69080e4a67fe6d7d816
西瓜° <wxh_email@yeah.net>
2021-12-02 18:01:31 +0800

    chore:集成lanzou-api

--- PublicReference/utils/LZextends.py
+++ PublicReference/utils/LZextends.py
@@ -3,19 +3,13 @@ from lanzou.api.types import *
 from typing import List
 
 
-class LZextends():
+class LZextends(LanZouCloud):
     def __init__(self):
         self.lzy = LanZouCloud()
         self.lzy._host_url = 'https://pan.lanzouo.com'
-
-    @staticmethod
-    def _all_possible_urls(url: str) -> List[str]:
-        """蓝奏云的主域名有时会挂掉, 此时尝试切换到备用域名"""
-        available_domains = [
-            # 'lanzouo.com',  # 鲁ICP备15001327号-8, 2021-09-15
-            'lanzouw.com',  # 鲁ICP备15001327号-7, 2021-09-02
-            'lanzoui.com',  # 鲁ICP备15001327号-6, 2020-06-09, SEO 排名最低
-            'lanzoux.com',  # 鲁ICP备15001327号-5, 2020-06-09
-            'lanzous.com'  # 主域名, 备案异常, 部分地区已经无法访问
+        self.lzy.available_domains = [
+            'lanzouo.com',  # 2021-09-15 鲁ICP备15001327号-8
+            'lanzouw.com',  # 2021-09-02 鲁ICP备15001327号-7
+            'lanzoui.com',  # 2020-06-09 鲁ICP备15001327号-6
+            'lanzoux.com',  # 2020-06-09 鲁ICP备15001327号-5
         ]
-        return [url.replace('lanzouo.com', d) for d in available_domains]
