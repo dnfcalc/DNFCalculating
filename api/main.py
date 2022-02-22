97e2d7d03d31d0dc551d8316183f5d704e53f8cb
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2022-02-22 20:36:08 +0800

    init

--- api/main.py
+++ api/main.py
@@ -1,15 +1,45 @@
 # api docs:https://fastapi.tiangolo.com/zh/tutorial/first-steps/
 
+import json
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+@app.get("/adventureinfo")
+async def get_adventure_info():
+    adventure_info = {}
+    with open("api/dataFiles/adventure_info.json",
+              encoding='utf-8') as fp:
+        adventure_info = json.load(fp)
+    return adventure_info
 
+
+@app.get("/balcklist")
+async def get_balcklist():
+    balcklist = {
+        "2190155ee92d17e8cc3b0c9892fd5ac7": {
+            "reason": "https://tieba.baidu.com/p/7624625617 不好意思 是计算器配不上你"
+        },
+        "99a2f094ef7daa0d53525b0f2474c0ea": {
+            "reason": "https://bbs.colg.cn/thread-8355814-1-1.html 带??节奏"
+        },
+        "1ba5ea8fa16964666f0c0a85e89c3e96": {
+            "reason": "https://bbs.colg.cn/thread-8358088-1-1.html 带节奏能请先把龙虎啸等级和其他计算搞清楚"
+        },
+        "2e3d28298db82f8b23dc9fa6aac14b6d": {
+            "reason": "https://bbs.colg.cn/thread-8358088-1-1.html 带节奏能请先把龙虎啸等级和其他计算搞清楚"
+        },
+        "f8b3cf5c269c97a8d6d2d97ecf769a06": {
+            "reason": "屠戮直播带节奏"
+        },
+        "761cf5dbd2e3d07e8eaaf1dc99ebc49b": {
+            "reason": "https://bbs.colg.cn/thread-8382566-1-1.html"
+        }
+    }
+    # 同步获取服务上的黑名单
+    return balcklist
 
 if __name__ == '__main__':
     uvicorn.run(
