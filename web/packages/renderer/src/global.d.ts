331b4b86c08042cb03bb1a8bbea6e28ad40fdb85
西瓜° <wxh_email@yeah.net>
2022-02-28 18:20:02 +0800

    init

--- web/packages/renderer/src/global.d.ts
+++ web/packages/renderer/src/global.d.ts
@@ -1,11 +1,14 @@
-
-export { }
+export {};
 
 declare global {
   interface Window {
     // Expose some Api through preload script
-    fs: typeof import('fs')
-    ipcRenderer: import('electron').IpcRenderer
-    removeLoading: () => void
+    fs: typeof import("fs");
+    ipcRenderer: import("electron").IpcRenderer;
+    server: {
+      statrServer: () => void;
+      stopServer: () => void;
+    };
+    removeLoading: () => void;
   }
 }
--- web/packages/renderer/src/main.ts
+++ web/packages/renderer/src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App)
-  .mount('#app')
-  .$nextTick(window.removeLoading)
+// 在vue3渲染前开启api服务
+window.server.statrServer();
+
+createApp(App).mount("#app").$nextTick(window.removeLoading);
 
 // console.log('fs', window.fs)
 // console.log('ipcRenderer', window.ipcRenderer)
 
 // Usage of ipcRenderer.on
-window.ipcRenderer.on('main-process-message', (_event, ...args) => {
-  console.log('[Receive Main-process message]:', ...args)
-})
+// window.ipcRenderer.on("main-process-message", (_event, ...args) => {
+//   console.log("[Receive Main-process message]:", ...args);
+// });
