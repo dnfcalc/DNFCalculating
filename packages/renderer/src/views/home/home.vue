e2d00979947ee3b7981aedd62353e7e55c696a84
西瓜° <wxh_email@yeah.net>
2022-03-03 17:00:00 +0800

    init

--- packages/renderer/src/views/home/home.vue
+++ packages/renderer/src/views/home/home.vue
@@ -3,10 +3,10 @@
     <template v-for="(sub, index) in adventureinfo">
       <div class="flex flex-row">
         <div
-          class="job-icon-box w-30 h-20 flex flex-wrap justify-center items-center relative bg-no-repeat bg-center"
+          class="job-icon-box w-30 h-25 flex flex-wrap justify-center items-center relative bg-no-repeat bg-center"
         >
           <div
-            class="w-15 h-15 bg-center bg-no-repeat"
+            class="w-30 h-22.5 bg-center bg-no-repeat"
             :style="sub_icon(index)"
           ></div>
         </div>
@@ -18,10 +18,10 @@
               class="job-border w-full h-full absolute z-2 bg-no-repeat"
             ></div>
             <div
-              class="job-mask bg-hex-#ffd7002e w-full h-full invisible z-999 absolute"
+              class="job-mask bg-hex-ffd7002e w-full h-full invisible z-999 absolute"
             ></div>
             <div
-              class="absolute bottom-1 w-full text-sm text-center text-hex-#bea347"
+              class="absolute bottom-1 w-full text-sm text-center text-hex-bea347"
             >
               {{ job.显示名称 }}
             </div>
